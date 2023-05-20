468a0ae41647b790c8d26f1de2136cab7d22812e
yugyeong71 <youkyoung71@naver.com>
2023-05-20 09:45:15 +0900

    1181 단어 정렬

---
+++ Lv. 13/WordSort1181.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+word = []
+
+for i in range(N):
+    word.append(sys.stdin.readline().strip())
+
+word_list = list(set(word))
+word_list.sort()
+word_list.sort(key=len)
+
+for i in word_list:
+    print(i)
